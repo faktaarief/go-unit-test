665ae7d7c30c31ba718f1ccea9be831700688e55
faktaarief <faktaarief16@gmail.com>
2021-10-25 05:54:14 +0700

    Added Before and After Test, Sub Test, Table Test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -7,6 +7,63 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestTableHelloWorld(t *testing.T) {
+	tests := []struct {
+		name     string
+		request  string
+		expected string
+	}{
+		{
+			name:     "Fakta",
+			request:  "Fakta",
+			expected: "Hello Fakta",
+		},
+		{
+			name:     "Arief",
+			request:  "Arief",
+			expected: "Hello Arief",
+		},
+		{
+			name:     "Farhanto",
+			request:  "Farhanto",
+			expected: "Hello Farhanto",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := HelloWorld(test.request)
+			require.Equal(t, test.expected, result)
+		})
+	}
+}
+
+func TestSubTest(t *testing.T) {
+	/*
+	* How to Running Test only on Sub Test
+	* $ go test -v -run=TestSubTest/Fakta
+	*
+	* How to Running Test contains "Fakta"
+	* $ go test -v -run=/Fakta
+	**/
+	t.Run("Fakta", func(t *testing.T) {
+		result := HelloWorld("Fakta")
+		require.Equal(t, "Hello Fakta", result, "Result must be 'Hello Fakta'")
+	})
+	t.Run("Arief", func(t *testing.T) {
+		result := HelloWorld("Arief")
+		require.Equal(t, "Hi Arief", result, "Result must be 'Hello Arief'")
+	})
+}
+
+func TestMain(m *testing.M) {
+	fmt.Println("BEFORE UNIT TEST")
+
+	m.Run()
+
+	fmt.Println("AFTER UNIT TEST")
+}
+
 func TestSkip(t *testing.T) {
 	// if runtime.GOOS == "windows" {
 	// 	t.Skip("Can not run on Winodws")
